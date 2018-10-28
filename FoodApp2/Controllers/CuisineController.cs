e240c18c696e6ff5edcf6c069896d99d13289837
ashwinusc93 <ashwincse93@outlook.com>
2018-10-28 22:01:18 +0530

    Action Results, Selectors, Filters

--- FoodApp2/Controllers/CuisineController.cs
+++ FoodApp2/Controllers/CuisineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodApp2.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,19 @@ using System.Web.Mvc;
 
 namespace FoodApp2.Controllers
 {
+    // Action and Controller Level Filters [Authorize]
+    [Log]
     public class CuisineController : Controller
     {
-        // GET: Cuisine
-        public ActionResult Search(string name)
+        public ActionResult Search(string name = "french")
         {
+            throw new Exception("Application Error");
+
             var message = Server.HtmlEncode(name);
+
+            //Action Result
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+            //return Json(new { Message = message, Name = "Ash" }, JsonRequestBehavior.AllowGet);
 
             return Content(message);
         }
