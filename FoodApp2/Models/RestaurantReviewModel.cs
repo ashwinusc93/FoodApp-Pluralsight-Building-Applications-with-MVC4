b947169c3979cdf9194ddf868c31704f67162267
ashwinusc93 <ashwincse93@outlook.com>
2018-11-03 03:30:31 +0530

    Custom Validations

--- FoodApp2/Models/RestaurantReviewModel.cs
+++ FoodApp2/Models/RestaurantReviewModel.cs
@@ -6,7 +6,31 @@ using System.Web;
 
 namespace FoodApp2.Models
 {
-    public class RestaurantReviewModel
+    // Approach 1 of Custom Validation (Attribute Specific Validation)
+    /*public class MaxWordsAttribute : ValidationAttribute
+    {
+        private int _maxWords;
+        public MaxWordsAttribute(int maxWords) : base("{0} has too many words")
+        {
+            _maxWords = maxWords;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var valueAsString = value.ToString();
+                if(valueAsString.Split(' ').Length > _maxWords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }*/
+    
+    public class RestaurantReviewModel : IValidatableObject
     {
         public int id { get; set; }
         [Range(1, 10)]
@@ -19,5 +43,13 @@ namespace FoodApp2.Models
         [StringLength(1024)]
         public string Body { get; set; }
         public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Rating < 2 && ReviewerName.ToLower().StartsWith("ash"))
+            {
+                yield return new ValidationResult("Sorry this is not possible");
+            }
+        }
     }
 }
